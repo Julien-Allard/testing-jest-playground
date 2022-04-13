a06e92853004431db144243b160746bf9b47b3f0
Julien-Allard <julalldev@gmail.com>
2022-04-13 19:04:59 +0200

    Feat: functionnality tests

--- src/components/login.js
+++ src/components/login.js
@@ -1,10 +1,43 @@
+import { useState } from "react";
+
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
   return (
     <div className="form-page">
       <form>
-        <input type="text" className="username" placeholder="Username" />
-        <input type="password" className="password" placeholder="password" />
-        <input type="button" className="submit-btn" value="Login" />
+        <input
+          type="text"
+          className="username"
+          placeholder="Username"
+          value={username}
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <input
+          type="password"
+          className="password"
+          placeholder="password"
+          value={password}
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
+        <input
+          type="button"
+          className="submit-btn"
+          value="Login"
+          disabled="true"
+        />
+        <p
+          className="error-message"
+          style={{ visibility: error ? "visible" : "hidden" }}
+        >
+          Something went wrong !
+        </p>
       </form>
     </div>
   );
--- src/components/login.test.js
+++ src/components/login.test.js
@@ -1,7 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Login from "./login";
 
+//TESTS FOR RENDERING AND CONTENT
 test("username placeholder input should be rendered", () => {
   render(<Login />);
   const usernamePlaceholder = screen.getByPlaceholderText(/username/i);
@@ -18,4 +19,47 @@ test("login button should be rendered", () => {
   render(<Login />);
   const loginButton = screen.getByRole("button");
   expect(loginButton).toBeInTheDocument();
+});
+
+test("username input should be empty on rendering", () => {
+  render(<Login />);
+  const usernameInput = screen.getByPlaceholderText(/username/i);
+  expect(usernameInput.value).toBe("");
+});
+
+test("password input should be empty on rendering", () => {
+  render(<Login />);
+  const passwordInput = screen.getByPlaceholderText(/password/i);
+  expect(passwordInput.value).toBe("");
+});
+
+//TEST FOR FUNCTIONNALITY
+test("if username or password are empty, button is disabled", () => {
+  render(<Login />);
+  const loginButton = screen.getByRole("button");
+  expect(loginButton).toBeDisabled();
+});
+
+test("error message should not be visible on rendering", () => {
+  render(<Login />);
+  const errorMessage = screen.getByText("Something went wrong !");
+  expect(errorMessage).not.toBeVisible();
+});
+
+test("username value should change on input", () => {
+  render(<Login />);
+  const usernameInput = screen.getByPlaceholderText(/username/i);
+  const testValue = "test";
+
+  fireEvent.change(usernameInput, { target: { value: testValue } });
+  expect(usernameInput.value).toBe(testValue);
+});
+
+test("password value should change on input", () => {
+  render(<Login />);
+  const passwordInput = screen.getByPlaceholderText(/password/i);
+  const testValue = "test";
+
+  fireEvent.change(passwordInput, { target: { value: testValue } });
+  expect(passwordInput.value).toBe(testValue);
 });
